5c43ade3aa3dc7e24895de2a9dd0c2905a6242aa
klh272 <54844341+klh272@users.noreply.github.com>
2021-07-19 12:42:22 +0200

    Add files via upload

--- pici_integrase_trimmer_script.py
+++ pici_integrase_trimmer_script.py
@@ -13,48 +13,45 @@ from Bio import SeqIO
 
 # read in tBLASTn alignment output
 df = pd.read_csv('tBLASTn_results.out', sep='\t', header = None)
+# create dictionary for fasta file
+record_dict = SeqIO.to_dict(SeqIO.parse("all.fna", "fasta"))
 
 # lists to hold headers and sequences for fasta output
 name_list = []
 seq_list = []
 description_list = []
 
-# read in sequences and run integrase search/trim
-fasta_file = 'all.fna' 
-with open(fasta_file, mode='r') as handle:
-  
-  for record in SeqIO.parse(handle, 'fasta'):
-    for i in range(len(df)):
-      if df.iloc[i,1].startswith(record.id): # ensures same sequence in multi-sequence fasta file is being used
-        if df.iloc[i,0].startswith('int'): # find integrases in df
-          if df.iloc[i,2] >= 90: # check it integrase identity is above 90%
-            print('\nIntegrase found with identity >= 90% in', record.description)
-            print('Location: {} {}'.format(df.iloc[i,8], df.iloc[i,9]))
-            print('BLAST database ID: ', df.iloc[i,0])
-            print('Beginning trim...')
+# search BLAST results for integrases
+for i in range(len(df)):
+    if df.iloc[i,0].startswith('int'): # find integrases in df
+      if df.iloc[i,2] >= 90: # check it integrase identity is above 90%
+        print('\nIntegrase found with identity >= 90% in', record_dict[df.iloc[i,1]].description)
+        print('Location: {} {}'.format(df.iloc[i,8], df.iloc[i,9]))
+        print('BLAST database ID: ', df.iloc[i,0])
+        print('Beginning trim...')
 
-            # get integrase start/end
-            int_start = df.iloc[i,8]
-            int_end = df.iloc[i,9]
+        # get integrase start/end
+        int_start = df.iloc[i,8]
+        int_end = df.iloc[i,9]
 
-            trim_low = int_start - 30000
-            trim_high = int_end + 30000
+        trim_low = int_start - 30000
+        trim_high = int_end + 30000
             
-            # fix lower limit index to 0 if it is negative
-            if trim_low < 0:
-              trim_low = 0
-            # fix upper limit index to the length of the sequence if it is over
-            if trim_high >= len(record.seq):
-              trim_high = len(record.seq)
+        # fix lower limit index to 0 if it is negative
+        if trim_low < 0:
+          trim_low = 0
+        # fix upper limit index to the length of the sequence if it is over
+        if trim_high >= len(record_dict[df.iloc[i,1]].seq):
+          trim_high = len(record.seq)
             
-            # Append trim to list
-            name_list.append(record.id)
-            seq_list.append(record.seq[trim_low:trim_high])
-            description_list.append(record.description)
-            print('Trim Finished.\n')
+        # Append trim to list
+        name_list.append(record_dict[df.iloc[i,1]].id)
+        seq_list.append(record_dict[df.iloc[i,1]].seq[trim_low:trim_high])
+        description_list.append(record_dict[df.iloc[i,1]].description)
+        print('Trim Finished.\n')
 
-        else:
-          print('No hit')
+    else:
+      print('No hit')
 
 
 # write trimmed sequence(s) to file
