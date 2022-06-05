a92f9a9cbb644a25445264a9a2a3edc2f7f9d9b1
klh272 <54844341+klh272@users.noreply.github.com>
2022-06-05 16:02:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,10 @@ mkdir scripts
 
 # set up databases
 wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_protein_db_new_integrases_21032022.fasta -P ./databases/putative -O BLAST_protein_db.faa
-makeblastdb -in ./databases/BLAST_protein_db.faa -input_type fasta -dbtype prot -out ./databases/PICI_BLAST_DB
+makeblastdb -in ./databases/putative/BLAST_protein_db.faa -input_type fasta -dbtype prot -out ./databases/putative/PICI_BLAST_DB
+wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_nucleotide_db.fna.fasta -P ./databases/derived -O BLAST_nucleotide_db.fna
+makeblastdb -in ./databases/derived/BLAST_protein_db.fna -input_type fasta -dbtype nucl -out ./databases/derived/PICI_BLAST_DB
+
 
 # download scripts
 wget https://raw.githubusercontent.com/klh272/pici-repository/main/pici_integrase_trimmer_script.py -P ./scripts/
