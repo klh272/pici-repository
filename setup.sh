5f73dab63a495928111c9cfcc80afbb789f1322c
klh272 <54844341+klh272@users.noreply.github.com>
2022-06-05 16:14:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,15 @@ mkdir pici_typer
 cd pici_typer
 mkdir data
 mkdir databases
+mkdir databases/putative
+mkdir databases/derived
 mkdir scripts
 
 
 # set up databases
-wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_protein_db_new_integrases_21032022.fasta -P ./databases/putative/ -O ./databases/putative/BLAST_protein_db.faa
+wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_protein_db_new_integrases_21032022.fasta -P ./databases/putative/ -O BLAST_protein_db.faa
 makeblastdb -in ./databases/putative/BLAST_protein_db.faa -input_type fasta -dbtype prot -out ./databases/putative/PICI_BLAST_DB
-wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_nucleotide_db.fna.fasta -P ./databases/derived/ -O ./databases/putative/BLAST_nucleotide_db.fna
+wget https://raw.githubusercontent.com/klh272/pici-repository/main/BLAST_nucleotide_db.fna.fasta -P ./databases/derived/ -O BLAST_nucleotide_db.fna
 makeblastdb -in ./databases/derived/BLAST_protein_db.fna -input_type fasta -dbtype nucl -out ./databases/derived/PICI_BLAST_DB
 
 
